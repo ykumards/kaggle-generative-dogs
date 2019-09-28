26d67f4868d8c52b77af3978048986b4e8940655
Yogesh Kumar <ykumar@nyu.edu>
2019-09-28 09:27:52 +0300

    Delete run_batch.sh

--- run_batch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --time=3:00:00
-#SBATCH --gres=gpu:v100:1
-#SBATCH --mem=32G
-
-module load anaconda3
-pip install pytorch-ignite --user
-srun ipython notebooks/dcgan-ignite-baseline-leval.py 
